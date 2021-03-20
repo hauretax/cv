5776c508d0f9fe189fd7b1016aecfc7a10a4d119
hauretax <31890474+hauretax@users.noreply.github.com>
2021-03-20 19:15:31 +0100

    petit bourde

--- src/component/Langages.js
+++ src/component/Langages.js
@@ -1,17 +1,17 @@
 import '../Style.css'
 
 export default function Langages() {
-    return(<div>
+    return (<div>
         <h3>Langages</h3>
         <hr />
-        <ul className = 'text'>
-            <li><div>(c, Javascript, MongoDB)</div>
-            <progress id="file" max="100" value="100" /> </li>
-            <li><div>(c++, css, Python, NodeJS, PHP, MySQL, git) </div
-            ><progress id="file" max="100" value="80" /></li>
-            <li><div>(MySQL, typescript, go, solidity)</div> 
-            <progress id="file" max="100" value="60" /></li>
+        <ul className='text'>
+            <li><div>(c, Javascript, Nodejs)</div>
+                <progress id="file" max="100" value="95" /> </li>
+            <li><div>(c++, css, Python, MongoDB, PHP, MySQL, git) </div>
+                <progress id="file" max="100" value="80" /></li>
+            <li><div>(MySQL, typescript, go, solidity)</div>
+                <progress id="file" max="100" value="60" /></li>
         </ul>
-        
+
     </div>)
 }
